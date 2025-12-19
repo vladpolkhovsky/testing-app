6a7f3706ed9ba67dcb073fb2dd0b06be013c4a9f
vladpolkhovsky <vlad.polkhovsky@gmail.com>
2025-12-19 23:09:17 +0300

    init

---
+++ backend/src/main/java/by/vppolkhovsky/tests_app/TestsAppApplication.java
@@ -0,0 +1,13 @@
+package by.vppolkhovsky.tests_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/by/vppolkhovsky/tests_app/TestsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vppolkhovsky.tests_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
